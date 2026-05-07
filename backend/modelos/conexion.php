1d5f512ad97d2d15fee3f6f9a12f235515b12d15
Camilobros <brayancamilodiaz3@gmail.com>
2026-05-07 10:25:09 -0500

    Update conexion.php

--- backend/modelos/conexion.php
+++ backend/modelos/conexion.php
@@ -5,7 +5,7 @@ $clave = "";
 $bd = "ventas";
 
 $conexion = mysqli_connect($servidor, $usuario, $clave) or die('no se conecto a mysql');
-mysqli_selec_db($conexion, $bd) or die('no se conecto a la base de datos ventas');
+mysqli_select_db($conexion, $bd) or die('no se conecto a la base de datos ventas');
 mysqli_set_charset($conexion, 'utf8'); //codificacion
 
 
