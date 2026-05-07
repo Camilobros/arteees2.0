bc18f7b3001f84639ab32b0738992d73f6b1b41a
Camilobros <brayancamilodiaz3@gmail.com>
2026-05-07 10:19:01 -0500

    Update obra.php

--- backend/controladores/obra.php
+++ backend/controladores/obra.php
@@ -3,10 +3,36 @@
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
     require_once('../modelos/conexion.php');
-    require_once('../modelos/producto.php');
+    require_once('../modelos/obra.php');
 
-    $control = $_GET['control']
+    $control = $_GET['control'];
+    $obra = new obra($conexion);
 
+    switch($control){
+        case 'consulta':
+            $vec = $obra->consulta();
+            break;
+        case 'insertar':
+            $json = file_get_contents('php://input');
+            $params = json_decode($json);
 
+            $vec = $obra->insertar($params);
+            break;
+        case 'editar':
+            $json = file_get_contents('php://input');
+            $id = $_GET['id'];
+            $params = json_decode($json);
 
+            $vec = $obra->editar($id, $params);
+            break;
+        case 'eliminar':
+            $id = $_GET['id'];
+
+            $vec = $obra->eliminar($id);
+            break;
+    }
+
+    $datos = json_encode($vec);
+    echo $datos;
+    header('Content-Type: application/json');
 ?>
