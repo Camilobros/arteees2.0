0b3f3ce9b4cf8a95129856d354a650f8c5c42260
Camilobros <brayancamilodiaz3@gmail.com>
2026-06-12 09:26:22 -0500

    creacion de los servicios

---
+++ frontend/src/app/servicios/usuario.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Usuario {
+  
+  url = "http://localhost/proyectos/arteees2.0/backend/controladores/usuario.php";
+
+  constructor(private http: HttpClient){};
+
+  consulta(){
+    return this.http.get(`${this.url}?control=consulta`);
+  }
+
+  insertar(params: any){
+    return this.http.post(`${this.url}?control=insertar`, JSON.stringify(params));
+  }
+
+  editar(id: number, params: any){
+    return this.http.post(`${this.url}?control=editar&id=${id}`, JSON.stringify(params));
+  }
+
+  eliminar(id: number){
+    return this.http.get(`${this.url}?control=eliminar&id=${id}`); 
+  }
+
+  
+}
