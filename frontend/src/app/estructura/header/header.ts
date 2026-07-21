5dd91bb4230a996d97975f39a984225141a8ad2a
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-21 09:52:47 -0500

    insertar cerrar sesion

--- frontend/src/app/estructura/header/header.ts
+++ frontend/src/app/estructura/header/header.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
+  nombre: any;
+  rol: any;
+
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    this.nombre = sessionStorage.getItem("nombre")
+    this.rol = sessionStorage.getItem("rol");
+  }
+
+  cerrar(){
+        sessionStorage.setItem("id", "");
+    sessionStorage.setItem("correo", "");
+    sessionStorage.setItem("nombre", "");
+    sessionStorage.setItem("rol", "");
+    this.router.navigate(['login']);
+
+  }
 
 }
