0ddec2ffdbcca5c631e3ca9203a53a35be44465f
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-22 09:25:59 -0500

    cerrar servicios segun rol del usuario

--- frontend/src/app/estructura/nav/nav.ts
+++ frontend/src/app/estructura/nav/nav.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
+
+  rol: any;
+
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    this.rol = sessionStorage.getItem('rol');
+  }
 
 }
