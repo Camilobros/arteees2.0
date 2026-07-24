1a3a671983dccad4c67ddd582730cb5c5ef14205
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-24 15:38:00 -0500

    vincular nuevos modulos

---
+++ frontend/src/app/modulos/pedidoinsertar/pedidoinsertar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidoinsertar',
+  imports: [],
+  templateUrl: './pedidoinsertar.html',
+  styleUrl: './pedidoinsertar.css',
+})
+export class Pedidoinsertar {
+
+}
