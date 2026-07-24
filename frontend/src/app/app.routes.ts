9d1f638588cea53cc1a42ca166385a614b8a6f72
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-24 13:27:14 -0500

    agregar modulos faltantes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { Productos } from './modulos/productos/productos';
 import { Login } from './modulos/login/login';
 import { NoEncontro } from './modulos/no-encontro/no-encontro';
 import { validaruserGuard } from './guard/validaruser-guard';
+import { Categoria } from './modulos/categoria/categoria';
+import { Pedido } from './modulos/pedido/pedido';
+import { Usuario } from './modulos/usuario/usuario';
 
 export const routes: Routes = [
     {
@@ -15,6 +18,10 @@ export const routes: Routes = [
             {path: 'dashboard', component: Dashboard , canActivate : [validaruserGuard]},
             {path: 'clientes', component: Clientes  , canActivate : [validaruserGuard]},
             {path: 'productos', component: Productos  , canActivate : [validaruserGuard]},
+            {path: 'categoria', component: Categoria  , canActivate : [validaruserGuard]},
+            {path: 'pedido', component: Pedido  , canActivate : [validaruserGuard]},
+            {path: 'usuario', component: Usuario  , canActivate : [validaruserGuard]},
+
             {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
             
         ]
