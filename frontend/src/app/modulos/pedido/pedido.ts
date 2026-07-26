ba0486b64175983506658d4672599fb17d8872b6
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-26 01:21:10 -0500

    pedido.html funciona la tabla!!!!

--- frontend/src/app/modulos/pedido/pedido.ts
+++ frontend/src/app/modulos/pedido/pedido.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Pedidoservice } from '../../servicios/pedidoservice';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-pedido',
@@ -13,7 +14,7 @@ export class Pedido {
 
   ventas: any;
 
-  constructor(private router: Router, private spedido: Pedidoservice) { }
+  constructor(private router: Router, private spedido: Pedidoservice,private cdr: ChangeDetectorRef) { }
   ngOnInit(): void {
     this.consulta();
 
@@ -22,6 +23,8 @@ export class Pedido {
   consulta() {
     this.spedido.consulta().subscribe((result: any) => {
       this.ventas = result;
+
+      this.cdr.detectChanges();
 
     });
 
