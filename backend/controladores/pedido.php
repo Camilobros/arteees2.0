986dd18e06d9a9b5cef8830123d00786331e15b0
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-25 21:21:33 -0500

    creacion de php/modulos/controladores

---
+++ backend/controladores/pedido.php
@@ -0,0 +1,39 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Content-Type: application/json');
+
+    require_once('../modelos/conexion.php');
+    require_once('../modelos/pedido.php');
+
+    $control = $_GET['control'];
+    $pedido = new Pedido($conexion);
+
+    switch($control){
+        case 'consulta':
+            $vec = $pedido->consulta();
+            break;
+        case 'insertar':
+            $json = file_get_contents('php://input');
+            $params = json_decode($json);
+
+            $vec = $pedido->insertar($params);
+            break;
+        case 'editar':
+            $json = file_get_contents('php://input');
+            $id = $_GET['id'];
+            $params = json_decode($json);
+
+            $vec = $pedido->editar($id, $params);
+            break;
+        case 'eliminar':
+            $id = $_GET['id'];
+
+            $vec = $pedido->eliminar($id);
+            break;
+    }
+
+    $datos = json_encode($vec, JSON_UNESCAPED_UNICODE);
+    echo $datos;
+
+?>
---
+++ backend/modelos/pedido.php
@@ -0,0 +1,82 @@
+<?php
+    class Pedido {
+        private $conexion;
+
+        public function __construct($conexion){
+            $this->conexion = $conexion;
+        }
+
+   
+        public function consulta(){
+            
+            $sql = "SELECT v.*, c.nombre AS nombre_cliente, a.nombre AS nombre_artista 
+                    FROM venta v
+                    LEFT JOIN cliente c ON v.id_cliente = c.id_cliente
+                    LEFT JOIN artista a ON v.id_artista = a.id_artista
+                    ORDER BY v.id_venta DESC;";
+            
+            $res = mysqli_query($this->conexion, $sql) or die('no se encontro la tabla venta');
+
+            $vec = [];
+
+            
+            while($row = mysqli_fetch_array($res)){
+                $vec[] = $row;
+            }
+
+            return $vec;
+        }
+
+        public function eliminar($id){
+            $sql = "DELETE FROM venta WHERE id_venta = $id";
+            mysqli_query($this->conexion, $sql) or die('no elimino el registro');
+
+            $vec = [];
+            $vec['resultado']  = "OK";
+            $vec['mensaje'] = "se elimino el registro";
+
+            return $vec;
+        }
+
+        public function insertar($params) {
+            
+            $id_cliente = $params->id_cliente;
+            $productos = $params->productos;
+            $subtotal = $params->subtotal;
+            $total = $params->total;
+            $id_artista = $params->id_artista;
+            
+            
+            $sql = "INSERT INTO venta (id_cliente, productos, subtotal, total, id_artista) 
+                    VALUES ($id_cliente, '$productos', $subtotal, $total, $id_artista)";
+            
+            mysqli_query($this->conexion, $sql) or die("Fallo MySQL: " . mysqli_error($this->conexion));
+            
+            $vec = [];
+            $vec['resultado'] = 'OK';
+            $vec['mensaje'] = 'se inserto el registro';
+            
+            return $vec;
+        }
+
+        public function editar($id, $params){
+            $sql = "UPDATE venta SET 
+                        id_cliente = $params->id_cliente, 
+                        productos = '$params->productos', 
+                        subtotal = $params->subtotal, 
+                        total = $params->total, 
+                        id_artista = $params->id_artista 
+                    WHERE id_venta = $id";
+            
+            mysqli_query($this->conexion, $sql) or die('no edito el registro');
+
+            $vec = [];
+            $vec['resultado']  = "OK";
+            $vec['mensaje'] = "se edito el registro";
+
+            
+            return $vec; 
+        }
+
+    }
+?>
