9929970ed5f805d46b742968236a7cf18c8f308f
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-26 01:15:43 -0500

    craacion de pedido.service

--- frontend/src/app/modulos/pedido/pedido.ts
+++ frontend/src/app/modulos/pedido/pedido.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pedido',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './pedido.css',
 })
 export class Pedido {
+
+  ventas : any;
+
+  constructor(private router: Router){}
+  ngOnInit(){
+    
+  }
+
+
+  
 
 }
