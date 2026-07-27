ca91c46cc424e940b28aada44a2c7cad33ce4960
Camilobros <brayancamilodiaz3@gmail.com>
2026-07-27 08:51:36 -0500

    configurando insertar pedido

--- frontend/src/app/modulos/pedidoinsertar/pedidoinsertar.ts
+++ frontend/src/app/modulos/pedidoinsertar/pedidoinsertar.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Obra } from '../../servicios/obra';
+import { Cliente } from '../../servicios/cliente';
+import { Pedidoservice } from '../../servicios/pedidoservice';
+import { CommonModule } from '@angular/common';  
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pedidoinsertar',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './pedidoinsertar.html',
   styleUrl: './pedidoinsertar.css',
 })
 export class Pedidoinsertar {
+
+  obras:any;
+  constructor(private router: Router, private sobra: Obra, private scliente: Cliente, private spedido : Pedidoservice) { }
+
+  ngOnInit(): void {
+    this.consulta_obra
+  }
+
+  consulta_obra(){
+    this.sobra.consulta().subscribe((result:any)=>{
+      this.obras = result;
+    })
+
+  }
 
 }
