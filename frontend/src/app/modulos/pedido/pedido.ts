bde8ac7875d68e2a0331a7821d6729b9da336f2f
Bryan Camilo Diaz Sanchez <brayancamilodiaz3@gmail.com>
2026-08-21 16:17:13 -0500

    crear ver detalles de pedido

--- frontend/src/app/modulos/pedido/pedido.ts
+++ frontend/src/app/modulos/pedido/pedido.ts
@@ -13,6 +13,9 @@ import { ChangeDetectorRef } from '@angular/core';
 export class Pedido {
 
   ventas: any;
+  modal = false;
+  productos: any;
+  total: any;
 
   constructor(private router: Router, private spedido: Pedidoservice,private cdr: ChangeDetectorRef) { }
   ngOnInit(): void {
@@ -30,10 +33,33 @@ export class Pedido {
 
   }
 
+  consultap(id:number){
+    this.spedido.consultap(id).subscribe((result: any) => {
+      this.productos = result;
+
+      this.cdr.detectChanges();
+
+    });
+
+  }
+
 
   insertar(){
     this.router.navigate(['pedidoins']);
 
+  }
+
+  mostrar_modal(dato:any, id:number){
+    switch(dato){
+      case 0:
+        this.modal = false;
+        break;
+        
+      case 1:
+        this.modal = true;
+        this.consultap(id);
+        break;  
+    }
   }
 
 
