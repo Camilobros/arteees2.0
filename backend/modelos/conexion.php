286cf35bc294f33eef889227dbb260c90fa1d114
Bryan Camilo Diaz Sanchez <brayancamilodiaz3@gmail.com>
2026-08-28 15:23:29 -0500

    intento 1.6

--- backend/modelos/conexion.php
+++ backend/modelos/conexion.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 $servidor = "api-artes.infinityfreeapp.com";
 $usuario = "if0_42772419";
 $clave = "LINNimay18";
