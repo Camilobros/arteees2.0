1b18d6ca9b94bca7ace37213e749c0c8f9e961b7
Bryan Camilo Diaz Sanchez <brayancamilodiaz3@gmail.com>
2026-08-28 15:12:50 -0500

    intento 1.4

--- backend/modelos/conexion.php
+++ backend/modelos/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$clave = "";
-$bd = "tienda_dibujos";
+$servidor = "api-artes.infinityfreeapp.com";
+$usuario = "if0_42772419";
+$clave = "LINNimay18";
+$bd = "if0_42772419_tienda_dibujos";
 
 $conexion = mysqli_connect($servidor, $usuario, $clave) or die('no se conecto a mysql');
 mysqli_select_db($conexion, $bd) or die('no se conecto a la base de datos ventas');
